225087b1eb5652f5148a5e4b5efaa5c5fa3cbb85
Irina Pyak <ir.pyak@gmail.com>
2021-05-04 22:44:11 -0400

    add fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var inputCity = "new%20york"
+var apiURL = "http://api.openweathermap.org/data/2.5/weather?appid=d91f911bcf2c0f925fb6535547a5ddc9&q="
 var cityInputEl = document.getElementById("weather-search")
 var formEl = document.getElementById("weather-form")
 // listen to the submit event on form
@@ -7,4 +9,15 @@ function handleSubmit(event){
     // read value from input 
     console.log(cityInputEl.value)
     // make ajax call to the weather API
+    fetch(apiURL + inputCity)
+        .then(processStream)
+        .then(logCityWeather)
+}
+
+function processStream (response){
+    return response.json()
+}
+
+function logCityWeather(data){
+    console.log(data)
 }
